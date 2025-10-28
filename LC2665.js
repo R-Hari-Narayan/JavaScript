92ee35d8ea11d22b2fd6d03355cfb6f6ccbbcdcf
R-Hari-Narayan <harinarayan2002@gmail.com>
2025-10-28 21:15:22 +0530

    LC2665.js

---
+++ LC2665.js
@@ -0,0 +1,26 @@
+// Counter II
+
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    const x = init
+    function increment() {
+        return ++init
+    }
+    function decrement() {
+        return --init
+    }
+    function reset() {
+        init = x
+        return init
+    }
+    return {increment, decrement, reset}
+};
+
+
+const counter = createCounter(5)
+console.log(counter.increment()); // 6
+console.log(counter.reset()); // 5
+console.log(counter.decrement()); // 4
